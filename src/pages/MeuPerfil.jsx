43c7f7cf4231a2de66411b60b163ca5c976a1299
Bernardo Santos Magalhaes <bernardosantosmagalhaesm@gmail.com>
2023-06-29 22:03:02 -0300

    2.0

--- src/pages/MeuPerfil.jsx
+++ src/pages/MeuPerfil.jsx
@@ -4,15 +4,15 @@ export const MeuPerfil = () => {
     return (
         <header className="flex  bg-[url('assets/img/Fundo.png')] opa bg-cover bg-center flex-col justify-center items-center h-full">
             <div className="backdrop-grayscale-0 bg-white/20">
-                <div className=" flex flex-col justify-center text-center pt-12 items-center h-full w-auto">
+                <div className=" flex flex-col justify-center text-center pt-4 items-center h-full w-auto">
 
                     <div className="bg-[url('assets/img/Perfil.jpg')]  flex-none bg-cover w-40 h-40 bg-bottom rounded-full  border-2 border-blue-300"> </div>
 
                     <div className="pt-4 flex-1 text-black font-semibold   ">
                         <div className=" text-sm flex flex-col ">
-                            <p className="text-lg font-bold flex-">Bernardo Santos </p>
+                            <p className="text-lg font-bold flex-auto">Bernardo Santos </p>
                             <p className="flex-1 -mt-10">Programador | Desenvolvedor</p>
-                            <div className="text-[8px] ">Contato: <span className="font-bold tracking-[0.7px]">bernardosantosmagalhaesm@gmail.com</span></div>
+                            <div className="text-[8px] flex-auto">Contato: <span className="font-bold tracking-[0.7px]">bernardosantosmagalhaesm@gmail.com</span></div>
                         </div>
                     </div>
 
